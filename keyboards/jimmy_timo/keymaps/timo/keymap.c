d17e66d475adbc191380d821af8f3ca1c0ebc54a
Timo <shik3i@protonmail.com>
2018-11-16 20:32:25 +0100

    anpassung

---
+++ keyboards/jimmy_timo/keymaps/timo/keymap.c
@@ -0,0 +1,19 @@
+#include "jimmy_timo.h"
+
+#define _MAIN 0
+#define _ALTGR 1
+#define _FN 2
+
+#define _______ KC_TRNS
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_MAIN] = LAYOUT(
+  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5
+)
+[_ALTGR] = LAYOUT(
+
+)
+[_FN] = LAYOUT(
+
+)
+};
