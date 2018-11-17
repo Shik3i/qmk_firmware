a1af03948630ac2715cf879dc711774aa7055012
Timo <shik3i@protonmail.com>
2018-11-17 20:58:36 +0100

    fixed missing argument

--- keyboards/timo/keymaps/default/keymap.c
+++ keyboards/timo/keymaps/default/keymap.c
@@ -12,19 +12,21 @@
 #define VolumeDown KC_AUDIO_VOL_DOWN
 
 
+// 64 Tasten insgesamt
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MAIN] = LAYOUT(
-  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,  KC_BSPC,
-  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,	KC_RBRC,
-  KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,	KC_QUOT, KC_NUHS, KC_ENT,
-  KC_LSHIFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_PSCR,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(_FN), KC_LEFT, KC_DOWN, KC_RIGHT
+  KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,  KC_BSPC, /*14*/
+  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,	KC_RBRC, /*13*/
+  KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,	KC_QUOT, KC_NUHS, KC_ENT, /*14*/
+  KC_LSHIFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_UP, KC_PSCR, /*14*/
+  KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(_FN), KC_LEFT, KC_DOWN, KC_RIGHT /*9*/
 ),
 [_FN] = LAYOUT(
-  KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,  ____,
-  ____, ____, ____, ____, ____, ____, ____, ____, ____, PrevTrack, PlayPause, NextTrack,	____,
-  RESET, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,	____, ____, ____,
-  ____, ____, ____, ____, ____ ____, ____, ____, AudioMute, ____, ____, ____, VolumeUp, KC_LOCKING_SCROLL,
-  ____, ____, ____, ____, ____, KC_NO, ____, VolumeDown, ____
+  KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,  ____, /*14*/
+  ____, ____, ____, ____, ____, ____, ____, ____, ____, PrevTrack, PlayPause, NextTrack,	____, /*13*/
+  RESET, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,	____, ____, ____, /*14*/
+  ____, ____, ____, ____, ____, ____, ____, ____, AudioMute, ____, ____, ____, VolumeUp, KC_LOCKING_SCROLL, /*14*/
+  ____, ____, ____, ____, ____, KC_NO, ____, VolumeDown, ____ /*9*/
 )
 };
