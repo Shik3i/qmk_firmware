2bb1ebc755d62d7c14d79a4e035ac46d3550221d
Timo <shik3i@protonmail.com>
2018-11-17 20:39:01 +0100

    added kc_trans in FN layer

--- keyboards/timo/keymaps/default/keymap.c
+++ keyboards/timo/keymaps/default/keymap.c
@@ -3,6 +3,8 @@
 #define _MAIN 0
 #define _FN 1
 
+#define ____ KC_TRANS
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_MAIN] = LAYOUT(
   KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL,  KC_BSPC,
@@ -12,10 +14,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(_FN), KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 [_FN] = LAYOUT(
-  KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,  KC_BSPC,
-  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,	KC_RBRC,
-  KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,	KC_QUOT, KC_NUHS, KC_ENT,
-  KC_LSHIFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_AUDIO_MUTE, KC_COMM, KC_DOT, KC_SLSH, KC_AUDIO_VOL_UP, KC_LOCKING_SCROLL,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_NO, KC_LEFT, KC_AUDIO_VOL_DOWN, KC_RIGHT
+  KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,  ____,
+  ____, ____, ____, ____, ____, ____, ____, ____, ____, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,	____,
+  RESET, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,	____, ____, ____,
+  ____, ____, ____, ____, ____ ____, ____, ____, KC_AUDIO_MUTE, ____, ____, ____, KC_AUDIO_VOL_UP, KC_LOCKING_SCROLL,
+  ____, ____, ____, ____, ____, KC_NO, ____, KC_AUDIO_VOL_DOWN, ____
 )
 };
