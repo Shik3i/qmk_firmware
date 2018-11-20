350f7fccce8131c767b262044baa8e3e967a0724
Timo <shik3i@protonmail.com>
2018-11-20 12:48:53 +0100

    test

--- keyboards/timo/config.h
+++ keyboards/timo/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Shik3iINC
 #define PRODUCT         Shorty
-#define DESCRIPTION     A short Keyboard
+#define DESCRIPTION     A short custom Keyboard
 
 /* key matrix size */
 #define MATRIX_ROWS 5
